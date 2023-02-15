038c0aafec0e0214d568164592caa61ddaa7fd6d
Vedansh Gupta <vedanshgupta606@gmail.com>
2023-02-15 23:37:52 +0530

    Create Solution.cpp

---
+++ Medium/Get minimum squares/Solution.cpp
@@ -0,0 +1,24 @@
+//User function Template for C++
+
+class Solution{
+	public:
+	int MinSquares(int n)
+	{
+	    // Code here
+	    if(n==0) return 0;
+	    if(n==1) return 1;
+	    vector<int> dp(n+1);
+	    dp[0] = 0;
+	    dp[1] = 1;
+	    for(int i=2;i<=n;i++){
+	        int mini = INT_MAX;
+	        for(int j=1;j*j<=i;j++){
+	            int val = i-(j*j);
+	            mini = min(mini,dp[val]);
+	           // if(dp[val]<mini) mini = dp[val];
+	        }
+	        dp[i]=mini+1;
+	    }
+	    return dp[n];
+	}
+};
