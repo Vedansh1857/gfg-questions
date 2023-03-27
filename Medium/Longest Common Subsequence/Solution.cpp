0b99c63c896ecca1db27f1fd9b5a5bf1c32f5db4
Vedansh Gupta <vedanshgupta606@gmail.com>
2023-03-28 00:23:51 +0530

    Update Solution.cpp

--- Medium/Longest Common Subsequence/Solution.cpp
+++ Medium/Longest Common Subsequence/Solution.cpp
@@ -22,12 +22,30 @@ class Solution
         dp[i][j] = max(textOne,textTwo);
         return dp[i][j];
     }
+    int solveTab(string& text1, string& text2){
+        vector<vector<int>> dp(text1.size()+1, vector<int> (text2.size()+1,0));
+        for(int i=text1.size()-1;i>=0;i--){
+            for(int j=text2.size()-1;j>=0;j--){
+                int ans = 0;
+                if(text1[i] == text2[j]) ans = dp[i+1][j+1]+1;
+                else{
+                    int textOne = dp[i+1][j];
+                    int textTwo = dp[i][j+1];
+                    ans = max(textOne,textTwo);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
     int lcs(int x, int y, string s1, string s2){
         // your code here
         
         // solveRec(s1,s2,0,0);
         
-        vector<vector<int>> dp(x+1, vector<int> (y+1,-1));
-        return solveMem(s1,s2,0,0,dp);
+        // vector<vector<int>> dp(x+1, vector<int> (y+1,-1));
+        // return solveMem(s1,s2,0,0,dp);
+        
+        return solveTab(s1,s2);
     }
 };
